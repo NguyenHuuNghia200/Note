de13628883f5448d4994f75b9b72de505a7868e9
NguyenHuuNghia200 <nghiats200@gmail.com>
2024-02-29 04:53:54 +0700

    cau hinh router

---
+++ ReactJs/src/router/index.jsx
@@ -0,0 +1,24 @@
+const AuthLayout = () => {
+    return (
+        <AuthProvider>
+            <Outlet />
+        </AuthProvider>
+    );
+};
+
+export default createBrowserRouter([
+    {
+        element: <AuthLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                element: <Login />,
+                path: '/login',
+            },
+            {
+                element: <Home />,
+                path: '/',
+            },
+        ]
+    }
+])
